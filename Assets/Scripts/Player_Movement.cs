949c124d603213ad55f0496403b502957812beb9
LD101 <jmuendongei@gmail.com>
2023-10-27 18:42:14 +0300

    Gravity_Run

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,34 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    #region Variables
+    //float
     [SerializeField] private float move_Speed;
     [SerializeField] private float gravity_Modifier;
+    //bool
     [SerializeField] private bool is_Grounded;
+    [SerializeField] private bool gravity_Switch;
+    [SerializeField] private bool jump;
+    [SerializeField] private bool gravity_Run;
+    //gravity modifier
     private Rigidbody rb;
-
+    #endregion
     private void Start()
     {
+        is_Grounded = false;
+        gravity_Switch = true;
         rb = GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
+        #region Is_Grounded
         if (collision.gameObject.CompareTag("Ground"))
         {
             is_Grounded = true;
+            gravity_Switch = false;
+            gravity_Run = true;
         }
+        #endregion //when is this true
     }
     void Update()
     {
-        if(is_Grounded == true)
+        Upward_Movement();
+    }
+
+    private void Upward_Movement()
+    {
+        if(gravity_Run == true)
         {
-            transform.Translate(Vector3.up * move_Speed);
-            if (Input.GetButtonDown("Jump"))
+            transform.Translate(Vector3.up * move_Speed); // uwpwards movement
+            if (is_Grounded == true && gravity_Run == true)
             {
-                Physics.gravity *= -1;
+                gravity_Run = true;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    Jump();
+                    gravity_Switch = true;
+                    is_Grounded = false;
+                    jump = true;
+                    gravity_Run = true;
+
+                }
+                if (!gravity_Switch)
+                {
+                    is_Grounded = true;
+                    gravity_Switch = false;
+                    jump = false;
+                }
             }
-        }
+        }  
+    }
+
+    private void Jump()
+    {
+        Physics.gravity *= -1; // changing gravity
+        jump = true;
+        gravity_Switch = true;
+        gravity_Run = true;
     }
 }
